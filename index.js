bab11654c09eda412c9581bc8552d5fbea392753
ASDA-Grocery <32156105+ASDA-Grocery@users.noreply.github.com>
2017-11-29 10:22:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,11 +12,28 @@ app.use(bodyParser.urlencoded({
 
 app.use(bodyParser.json());
 
-app.post('/test', function(req, res) {
+app.post('/analyze', function(req, res) {
     var speech
       , messages = new Array()
       , intent = req.body.result && req.body.result.metadata.intentName ? req.body.result.metadata.intentName : "noIntent";
-
+    if(intent === 'UncertainIntent'){
+        console.log('Inside Uncertain Intent')
+        speech = 'Incide Uncertain Intent'
+        return res.json({
+            speech: speech,
+            displayText: speech,
+            source: 'sentiment-analyzer-backend'
+        });
+    }
+    else{
+        console.log('No intent received')
+        speech = 'No intent Received'
+        return res.json({
+            speech: speech,
+            displayText: speech,
+            source: 'sentiment-analyzer-backend'
+        });
+    }
 });
 
 app.listen((process.env.PORT || 8000), function() {
